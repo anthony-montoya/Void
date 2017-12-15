f7fa8d0645ed2fd3e57da099f08db0da0bdce96b
Alex Hill <big_al_hill@comcast.net>
2017-12-15 15:34:45 -0700

    styling login box

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
-
+import './Login.css'
 class Login extends Component {
     render() {
         return (
-            <div>
-                GIVE ME YOUR INFO PUNK.
+            <div className='login-wrapper'>
+                <div className='login-box'>
+                    <div className='sign-up'> SIGN UP </div>
+                    <div className='username'><div className='input-id'> UserName* </div>   <input className='username-input' type='text' /> </div>
+                    <div className='username'><div className='input-id'> Email* </div>   <input className='username-input' type='text' /> </div>
+                    <div className='username'><div className='input-id'> Password* </div> <input className='username-input' type='text' /> </div>
+                    <div className='username'><div className='input-id'> Uplay UserName* </div> <input className='username-input' type='text' /> </div>
+
+
+                </div>
             </div>
         );
     }
