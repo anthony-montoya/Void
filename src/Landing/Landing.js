26651b69a3d5f181c1df599f3f3fc10a6e420098
Alex Hill <big_al_hill@comcast.net>
2017-12-18 15:18:44 -0700

    Added nav bar

--- src/Landing/Landing.js
+++ src/Landing/Landing.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import './Landing.css'
 
 class Landing extends Component {
     render() {
         return (
             <div>
-                <p>VOID BATTLES LANDING PAGE BASIC COMPONENT READY TO GO.</p>
+                <div className='Left-side-nav-bar'>
+                    <div className='logo-wrapper'>   
+                        <div className='logo'> <span className='void-white'>VOID-</span><span className='void-red'>BATTLES</span> </div>
+                    </div> 
+                    <div className='signup-login-wrapper'>   
+                        <div className='nav-links'> Login/Sign-Up </div>
+                    </div>
+                    <div className='nav-links'> Profile </div>
+                    <div className='nav-links'> Dashboard </div>
+                </div> 
             </div>
         );
     }
