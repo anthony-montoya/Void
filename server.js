bbd6b13d5844b46a6a08fdae9b7526199afda7cf
Samuel-Montoya <samuel@ostlergroup.com>
2018-03-12 20:39:06 -0600

    Added styled-components

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bodyParser  = require('body-parser');
+const bodyParser = require('body-parser');
 const cors = require('cors')
 const massive = require("massive");
 
@@ -12,19 +12,31 @@ massive({
     port: '5432',
     database: 'df7e37aantqni4',
     user: 'gpspagsoacswgw',
-    password: '3d394f39d802306192923a07a168928658d4dab713f75d2b30a33897597e8874',  
+    password: '3d394f39d802306192923a07a168928658d4dab713f75d2b30a33897597e8874',
     ssl: true
 }).then(function (db) {
     app.set('db', db)
 })
 
 
-app.post('/register_user', (req, res) => {
-    const {username, email, password, uplay} = req.body;
-    req.app.get('db').create_user([username, email, password, uplay]).then( response => {
-        res.status(200).send('User logged in')
-    } )
+// var { Client } = require('pg')
+// const client = new Client({ connectionString: process.env.DATABASE_URI })
 
+// try {
+//     client.connect((error, client, done) => {
+//       if (error) console.log(('Failed to connect to database...' + error).error)
+//       else console.log('Connected to database!'.bold.white)
+//     })
+//   } catch (err) {
+//     console.log('Failed to Connect to Database: ', err)
+//   }
+
+
+app.post('/register_user', (req, res) => {
+    const { username, email, password, uplay } = req.body;
+    req.app.get('db').create_user([username, email, password, uplay]).then(response => {
+        res.status(200).send('User logged in')
+    })
 })
 
 
