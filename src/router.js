724f5064b0f4d9ccf11a39a1ffdab1d8587c0727
TOG-Group <samuel@ostergroup.com>
2018-03-14 09:21:01 -0600

    Fixed merge conflicts

--- src/router.js
+++ src/router.js
@@ -1,34 +1,16 @@
 import React from 'react'
-<<<<<<< HEAD
-import {Switch, Route} from 'react-router-dom'
-import Landing from './components/Landing/Landing.js'
+import { Switch, Route } from 'react-router-dom'
+import Landing from './components/Landing/Landing.jsx'
 import Login from './components/Login/Login.jsx'
 import Profile from './components/Profile/Profile.jsx'
-import {Authentication} from './components/Authentication'
-
-
-
-
-export default (
-    <Switch>
-        <Route component={Landing} path='/' exact/>
-        <Route component={Login} path='/login' />
-        <Route component={Authentication(Profile)} path='/profile' />
-    </Switch>
-)
-=======
-import { Switch, Route } from 'react-router-dom'
-import Landing from './components/Landing/Landing'
-import Login from './components/Login/Login'
-import Profile from './components/Profile/Profile'
+import { Authentication } from './components/Authentication'
 import NotFound from './components/PageNotFound/PageNotFound'
 
 export default (
 	<Switch>
 		<Route component={Landing} path="/" exact />
 		<Route component={Login} path="/login" />
-		<Route component={Profile} path="/profile" />
+		<Route component={Authentication(Profile)} path="/profile" />
 		<Route component={NotFound} path="*" />
 	</Switch>
 )
->>>>>>> Feature/Landing
