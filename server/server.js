697e356e9d6db9cb4e78e1b6b952d5880c254767
Anthony Montoya <a.montoya1073@gmail.com>
2018-09-21 17:37:09 -0600

    Set up express server with a connection to Voids DBD mongodb instance

--- server/server.js
+++ server/server.js
@@ -1,27 +1,21 @@
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 const cors = require('cors')
-const massive = require("massive");
-const jwt = require('jsonwebtoken')
-const loginCtrl = require('./controllers/loginCtrl')
 
-const app = express();
+const app = express()
 app.use(bodyParser.json())
 app.use(cors())
 
+mongoose.connect('mongodb://admin:test123@ds243085.mlab.com:43085/void_dbd', { useNewUrlParser: true })
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('HIT')
+});
 
 
-
-
-app.post('/register_user', loginCtrl.register)
-
-app.get('/login_user/:vb_username/:password', loginCtrl.login)
-app.get('/authenticateAuthToken/:token', loginCtrl.authenticateToken)
-
-
-
-
-app.listen('4000', console.log('ayyee listen fam on 4000'))
-
-
+const port = process.env.PORT
+app.listen(port, () => console.log(`Reporting for duty on port ${port}`))
